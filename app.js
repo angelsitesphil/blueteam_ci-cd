b09dc2a17059444ff4dbd2576e67f38c8f1b35f3
angelsitesphil <markangelsantos724@gmail.com>
2025-11-06 17:13:36 +0800

    @nd Comit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require("http");
-const port = 3000;
+const port = 4000;
 
-http
-  .createServer((req, res) => {
-    res.end("Hello from Simple DevSecOps PoC on AWS Linux!");
-  })
-  .listen(port, () => console.log("Server running on port " + port));
+http.createServer((req, res) => {
+  res.end("Hello from BlueTeam CI/CD!");
+}).listen(port, () => {
+  console.log("Server running on port " + port);
+});
